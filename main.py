64987c52c42dcebded742be9f66f19f215cc196f
amir miri <saturnloop.ai@gmail.com>
2025-12-12 01:40:32 +0330

    add get current weather for the openmeteoweatherssss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #modules and global variables
 from  abc import ABC, abstractmethod
+import requests
 
 class WeatherAbstract(ABC):
     @abstractmethod
@@ -24,4 +25,11 @@ class OpenMeteoWeatherProvider(WeatherAbstract):
     base_url ="https://api.open-meteo.com/v1/forecast?"
 
     def get_current_weather(self, latitude, longitude):
-        respond  = requests.get(self.base_url + "&lat=" + str(latitude) + "&lon=" + str(longitude))
+        params = {
+        "latitude" :latitude ,
+        "longitude" :longitude ,
+        "current" : "temperature_2m,relative_humidity_2m"
+        }
+        respond  = requests.get(self.base_url, params = params  )
+        normalize_data = {"temp": respond.json()["current"]["temperature_2m"] , "humidity": respond.json()["current"]["relative_humidity_2m"] }
+        return normalize_data
