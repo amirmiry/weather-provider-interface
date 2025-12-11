bec273a0f1379994ace26c441fce09c4fac5e4e1
amir miri <saturnloop.ai@gmail.com>
2025-12-11 22:56:38 +0330

    make OpenWeatherProvider class (child)

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ class WeatherAbstract(ABC):
     @abstractmethod
     def get_current_weather(self , latitude, longitude):
         pass
+
+
+
+
+class OpenWeatherProvider(WeatherAbstract):
+
+    def __init__(self,  api_key):
+        self.api_key = api_key
+
+
+    def get_current_weather(self , latitude, longitude ):
+        pass
+
